de9fcd3f5d6fb648357177c7de4386b45e308617
Hasan1971f <mdhasanurrahmanhasan101@gmail.com>
2024-12-24 18:26:00 +0600

    added a  toggle show password and accept terms

--- src/Components/SignUp/SignUp.jsx
+++ src/Components/SignUp/SignUp.jsx
@@ -1,25 +1,34 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../firebase.init";
 import { useState } from "react";
+import { FaEye } from "react-icons/fa6";
+import { FaRegEyeSlash } from "react-icons/fa";
 
 
 const SignUp = () => {
     const [success, setSuccess] = useState(false)
-    const[errorMessage, setErrorMessage] = useState('')
-   
-    const handleSignUp = e =>{
-         e.preventDefault()
-        
-         const email = e.target.email.value
-         const password = e.target.password.value
-         console.log(email, password)
+    const [errorMessage, setErrorMessage] = useState('')
+    const [showPassword, setShowPassword] = useState(false)
+
+    const handleSignUp = e => {
+        e.preventDefault()
+
+        const email = e.target.email.value
+        const password = e.target.password.value
+        const terms = e.target.terms.checked
+        console.log(email, password, terms)
 
         //  reset error and status
 
         setErrorMessage('')
-        
-      
-        if(password.length <6){
+
+        if (!terms) {
+            setErrorMessage('Please acceept our terms and conditions')
+            return
+        }
+
+
+        if (password.length < 6) {
             setErrorMessage('Password should be 6 cahracters or long')
             return
         }
@@ -32,23 +41,23 @@ const SignUp = () => {
         }
 
 
-    //    create user with email and password
-       createUserWithEmailAndPassword(auth, email, password)
-       .then(result =>{
-           console.log(result.user)
-           setSuccess(true)
-          
-       })
-      .catch(error =>{
-        console.log("ERROR", error.message)
-        setErrorMessage(error.message)
-        setSuccess(false)
-       
-      
-      })
+        //    create user with email and password
+        createUserWithEmailAndPassword(auth, email, password)
+            .then(result => {
+                console.log(result.user)
+                setSuccess(true)
+
+            })
+            .catch(error => {
+                console.log("ERROR", error.message)
+                setErrorMessage(error.message)
+                setSuccess(false)
+
+
+            })
     }
-    
-    
+
+
     return (
         <div className="card bg-base-100 w-full max-w-sm  mx-auto shrink-0 shadow-2xl">
             <h3 className="text-3xl font-bold">Sign Up now!</h3>
@@ -59,25 +68,41 @@ const SignUp = () => {
                     </label>
                     <input type="email" name="email" placeholder="email" className="input input-bordered" required />
                 </div>
-                <div className="form-control">
+                <div className="form-control relative">
                     <label className="label">
                         <span className="label-text">Password</span>
                     </label>
-                    <input type="password" name="password" placeholder="password" className="input input-bordered" required />
+                    <input
+                        type={showPassword ? 'text' : 'password'}
+                        name="password"
+                        placeholder="password"
+                        className="input input-bordered" required />
+                    <button onClick={() => setShowPassword(!showPassword)} className="btn btn-xs absolute right-4 top-12">
+                        {
+                            showPassword ? <FaRegEyeSlash></FaRegEyeSlash> : <FaEye></FaEye>
+                        }
+                    </button>
                     <label className="label">
                         <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                    </label>
+                </div>
+                <div className="form-control">
+                    <label className="label justify-start cursor-pointer">
+                    <input type="checkbox" name="terms" className="checkbox" />
+                        <span className="label-text ml-2">Accept our terms and conditions</span>
+                        
                     </label>
                 </div>
                 <div className="form-control mt-6">
                     <button className="btn btn-primary">Sign Up</button>
                 </div>
             </form>
-           {
-            errorMessage && <p className="text-sky-400">{ errorMessage}</p>
-           }
-           {
-            success && <p className="text-green-600">Signup is successfully</p>
-           }
+            {
+                errorMessage && <p className="text-sky-400">{errorMessage}</p>
+            }
+            {
+                success && <p className="text-green-600">Signup is successfully</p>
+            }
         </div>
 
     );
